a459b8fc92dcd39afd50175b012ec69c006f6b7b
Hyeong jin Lee <hyeolee@c6r2s2.42seoul.kr>
2021-05-23 16:49:46 +0900

    connect wordpress to mysql

--- srcs/requirements/mariadb/mysql_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp ./tmp/my.cnf etc/
-
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON *.* TO 'hyeolee'@'%' IDENTIFIED BY '1111' WITH GRANT OPTION" | mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
-
-bash
