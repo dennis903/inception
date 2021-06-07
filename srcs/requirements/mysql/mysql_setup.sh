3dde5f75d5b5282e8b5556ff06f0a1d1b6dc9ed8
Hyeong jin Lee <hyeolee@c6r8s4.42seoul.kr>
2021-06-07 20:40:17 +0900

    wow

--- srcs/requirements/mysql/mysql_setup.sh
+++ srcs/requirements/mysql/mysql_setup.sh
@@ -4,7 +4,10 @@ cp -rp ./tmp/my.cnf   ./etc/mysql/my.cnf
 service mysql start
 chown -R mysql:mysql /var/lib/mysql
 chown -R mysql:mysql /var/log/mysql
-mysql -u root -pwordpress < cmd.sql
+#mysql -u root -pwordpress < cmd.sql
+
+mysql < cmd.sql
+mysql -u root -pwordpress < wordpress.sql
 ps -e | grep mysqld | awk '{print $1}' > a
 kill -kill `cat a`
 sleep 2
